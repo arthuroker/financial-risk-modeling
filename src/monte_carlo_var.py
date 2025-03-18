09188eedded533811cefb30a30ebb36accc96252
arthuroker <okerarthur@gmail.com>
2025-03-17 21:58:16 -0400

    got rid of debugging values

--- src/monte_carlo_var.py
+++ src/monte_carlo_var.py
@@ -7,19 +7,13 @@ def monte_carlo_var(initial_price, returns, confidence_level = 95, num_simulatio
   mean_return = returns.mean()
 
   standard_deviation = returns.std()
-
-  print(f"Mean return: {mean_return:.6f}")
-  print(f"Standard deviation: {standard_deviation:.6f}")
-  print(f"Initial price: {initial_price:.2f}")
-
+  
   delta_t = 1
 
   random_shocks = np.random.normal(0, 1, num_simulations)
   simulated_returns = mean_return - (.5 * (standard_deviation**2)) * delta_t + standard_deviation * random_shocks * np.sqrt(delta_t)
 
   simulated_prices = initial_price * np.exp(simulated_returns)
-
-  print(f"Simulated prices (first 5): {simulated_prices[:5]}")
 
   simulated_losses = (simulated_prices - initial_price) / initial_price
 
